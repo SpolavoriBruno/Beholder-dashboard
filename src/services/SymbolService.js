7293606fbef83b26e18b612c9b3506fd17995d8c
Bruno J. Spolavori <bspol.323@gmail.com>
2022-04-14 00:06:38 -0300

    Create Symbols Service

---
+++ src/services/SymbolService.js
@@ -0,0 +1,31 @@
+import axios from 'axios';
+
+const API_URL = process.env.REACT_APP_API_URL;
+
+export async function getSymbols(token) {
+    const url = `${API_URL}/symbols`;
+    const headers = {
+        'authorization': token
+    }
+
+    const response = await axios.get(url, { headers });
+    return response.data;
+}
+
+export async function getSymbol(symbol, token) {
+    const url = `${API_URL}/symbols/${symbol}`;
+    const headers = {
+        'authorization': token
+    }
+    const response = await axios.get(url, { headers });
+    return response.data;
+}
+
+export async function updateSymbol(symbolData, token) {
+    const url = `${API_URL}/symbols/${symbolData.symbol}`;
+    const headers = {
+        'authorization': token
+    }
+    const response = await axios.patch(url, symbolData, { headers });
+    return response.data;
+}
