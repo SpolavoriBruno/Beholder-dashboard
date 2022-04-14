79e3f8da1d4bf6d128463013f5841036a255dcfa
Bruno J. Spolavori <bspol.323@gmail.com>
2022-04-13 23:57:10 -0300

    Create 'Settings Service'

---
+++ src/services/SettingsService.js
@@ -0,0 +1,21 @@
+import axios from "axios"
+
+const API_URL = process.env.REACT_APP_API_URL
+
+export async function getSettings(token) {
+    const settingsUrl = `${API_URL}/settings`
+    const headers = {
+        'authorization': token
+    }
+    const response = await axios.get(settingsUrl, { headers })
+    return response.data
+}
+
+export async function updateSettings(settings, token) {
+    const settingsUrl = `${API_URL}/settings`
+    const headers = {
+        'authorization': token
+    }
+    const response = await axios.patch(settingsUrl, settings, { headers })
+    return response.data
+}
