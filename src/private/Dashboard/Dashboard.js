d130fbaaa55ea161a8b421ba4946c6798ec85931
Bruno José Spolavori <bspol.323@gmail.com>
2022-04-18 01:35:09 -0300

    Implement Wallet component

--- src/private/Dashboard/Dashboard.js
+++ src/private/Dashboard/Dashboard.js
@@ -3,10 +3,12 @@ import useWebSocket from "react-use-websocket";
 
 import MiniTicker from "./MiniTicker/MiniTicker";
 import BookTicker from "./BookTicker/BookTicker";
+import Wallet from "./Wallet/Wallet";
 
 function Dashboard() {
     const [miniTickerState, setMiniTickerState] = useState({})
     const [bookState, setBookState] = useState({})
+    const [balanceState, setBalanceState] = useState({})
 
     const { lastJsonMessage } = useWebSocket(process.env.REACT_APP_WS_URL, {
         onOpen: () => console.info('Connected to WS Server'),
@@ -14,17 +16,22 @@ function Dashboard() {
             if (lastJsonMessage) {
                 if (lastJsonMessage.miniTicker)
                     setMiniTickerState(lastJsonMessage.miniTicker)
+
+                if (lastJsonMessage.balance)
+                    setBalanceState(lastJsonMessage.balance)
+
                 if (lastJsonMessage.book) {
                     lastJsonMessage.book.forEach(b => bookState[b.symbol] = b)
                     setBookState(bookState)
                 }
+
             }
         },
         onError: error => console.error(error),
         shouldReconnect: closeEvent => true,
         reconnectInterval: 3000,
         reconnectAttempts: 20,
-
+        queryParams: { token: localStorage.getItem('token') }
     })
 
     return (<React.Fragment>
@@ -33,8 +40,9 @@ function Dashboard() {
                 <h1 className="h4">Dashboard</h1>
             </div>
             <MiniTicker data={miniTickerState} />
-            <BookTicker data={bookState} />
             <div className="row">
+                <BookTicker data={bookState} />
+                <Wallet data={balanceState} />
             </div>
         </main>
     </React.Fragment>)
---
+++ src/private/Dashboard/Wallet/Wallet.js
@@ -0,0 +1,61 @@
+import React, { useEffect, useState } from "react"
+import { getBalance } from "../../../services/ExchangeService"
+import "../Dashboard.css"
+
+export default function Wallet(props) {
+
+    const [balances, setBalances] = useState([])
+
+
+    useEffect(() => {
+        const token = localStorage.getItem('token')
+        getBalance(token)
+            .then(info => {
+                const balances = Object.entries(info).map(item => {
+                    return {
+                        symbol: item[0],
+                        available: item[1].available,
+                        onOrder: item[1].onOrder,
+                    }
+                })
+
+                setBalances(balances)
+            })
+            .catch(console.error)
+
+    }, [props.data])
+
+    if (!props) return (<React.Fragment></React.Fragment>)
+
+    return (<React.Fragment>
+        <div className="col-sm-12 col-md-6 mb-4">
+            <div className="card border-0 shadow">
+                <div className="card-header">
+                    <h2 className="fs-5 fw-bold mb-0">Book</h2>
+                </div>
+                <div className="table-responsive divScroll">
+                    <table className="table align-items-center table-flush table-sm table-hover tableFixHead">
+                        <thead className="thead-light">
+                            <tr>
+                                <th className="border-bottom" scope="col">SYMBOL</th>
+                                <th className="border-bottom col-2" scope="col">FREE</th>
+                                <th className="border-bottom col-2" scope="col">LOCK</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {
+                                balances?.map(item => (
+                                    <tr key={`wallet/${item.symbol}`}>
+                                        <td className="text-gray-900">{item.symbol}</td>
+                                        <td className="text-gray-900">{item.available.substring(0, 8)}</td>
+                                        <td className="text-gray-900">{item.onOrder.substring(0, 8)}</td>
+                                    </tr>
+                                ))
+                            }
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </React.Fragment>);
+}
