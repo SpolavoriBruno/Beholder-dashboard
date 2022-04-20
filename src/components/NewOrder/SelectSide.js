63883b303245215b9cf38cc31e2acb7d1b210c4e
Bruno José Spolavori <bspol.323@gmail.com>
2022-04-20 01:53:47 -0300

    Create Side Select component

---
+++ src/components/NewOrder/SelectSide.js
@@ -0,0 +1,14 @@
+import React, { useMemo } from "react"
+
+function SelectSide(props) {
+    return useMemo(() => (<div className="form-group">
+        <label htmlFor="side">Side</label>
+        <select className="form-select" id="side" defaultValue={props.side} onChange={props.onChange}>
+            <option value="BUY">Buy</option>
+            <option value="SELL">Sell</option>
+        </select>
+
+    </div>), [props.side, props.onChange])
+}
+
+export default SelectSide
