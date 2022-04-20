c8f61dbde2c63cbe831538e64c939576bc01f75b
Bruno José Spolavori <bspol.323@gmail.com>
2022-04-20 01:41:13 -0300

    Fix reconnect function

--- src/components/NewOrder/SymbolPrice.js
+++ src/components/NewOrder/SymbolPrice.js
@@ -24,7 +24,7 @@ function SymbolPrice(props) {
             }
         },
         onError: console.error,
-        shouldReconnect: true,
+        shouldReconnect: closeEvent => true,
         reconnectInterval: 3000,
         reconnectAttempts: 20
     })
