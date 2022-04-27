6f0d58ebf22bca32b1121adb7c66f897c8e01250
Bruno José Spolavori <bspol.323@gmail.com>
2022-04-27 15:42:23 -0300

    Remove "magical words"

--- src/services/ExchangeService.js
+++ src/services/ExchangeService.js
@@ -4,6 +4,21 @@ const API_URL = process.env.REACT_APP_API_URL
 
 export const STOP_TYPES = ["STOP_LOSS", "STOP_LOSS_LIMIT", "TAKE_PROFIT", "TAKE_PROFIT_LIMIT"]
 
+export const ORDER_SIDE = {
+    BUY: "BUY",
+    SELL: "SELL"
+}
+
+export const ORDER_TYPES = {
+    ICEBERG: "ICEBERG",
+    MARKET: "MARKET",
+    LIMIT: "LIMIT",
+    STOP: "STOP",
+    STOP_LIMIT: "STOP_LIMIT",
+    TAKE_PROFIT: "TAKE_PROFIT",
+    TAKE_PROFIT_LIMIT: "TAKE_PROFIT_LIMIT"
+}
+
 export async function getBalance(token) {
     const balanceUrl = `${API_URL}/exchange/balance`
     const headers = {
