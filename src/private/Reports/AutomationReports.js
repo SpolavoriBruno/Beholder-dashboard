c69634c3cdbce39ff04576a87a06af2d799691b7
Bruno José Spolavori <bspol.323@gmail.com>
2022-05-26 00:41:13 -0300

    Create component

---
+++ src/private/Reports/AutomationReports.js
@@ -0,0 +1,46 @@
+import React, { useEffect, useState } from "react"
+
+const style = {
+    overflow: 'auto',
+    'min-width': '550px',
+}
+
+export default function AutomationReports({ data }) {
+
+    const [automations, setAutomations] = useState([])
+
+    useEffect(() => {
+        if (!data) return
+        setAutomations(data)
+    }, [data])
+
+    return (<div className="col mb-4" style={style}>
+        <div className="card border-0 shadow">
+            <div className="card-header">
+                <h2 className="fs-5 fw-bold mb-0">Automations</h2>
+            </div>
+            <div className="table-responsive divScroll">
+                <table className="table align-items-center table-flush table-sm table-hover tableFixHead">
+                    <thead className="thead-light">
+                        <tr>
+                            <th className="border-bottom" scope="col">Name</th>
+                            <th className="border-bottom" scope="col">Exec.</th>
+                            <th className="border-bottom" scope="col">Net</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            Array.isArray(automations) && automations.map(item => (
+                                <tr key={`wallet/${item.name}`}>
+                                    <td className="text-gray-900">{item.name}</td>
+                                    <td className="text-gray-900">{item.executions}</td>
+                                    <td className="text-gray-900">{item.net.toFixed(2)}</td>
+                                </tr>
+                            ))
+                        }
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>)
+}
